251f0df31c88b84ff8c8ec32c751e4e629e100d1
Carlos Gavidia-Calderon <cgavidia-calderon@turing.ac.uk>
2024-08-26 16:56:02 +0100

    Solucion al problema dos lista

--- laboratorio.py
+++ laboratorio.py
@@ -1,6 +1,9 @@
+import math
+
+
 class TarjetaDeCredito:
-    
-    def __init__(self,nombre, numero_tarjeta, limite, balance ):
+
+    def __init__(self, nombre, numero_tarjeta, limite, balance):
         self.nombre = nombre
         self.numero_tarjeta = numero_tarjeta
         self.limite = limite
@@ -9,10 +12,12 @@ class TarjetaDeCredito:
     def comprar(self, monto):
 
         if (self.balance + monto) <= self.limite:
-            self.balance = self.balance + monto 
+            self.balance = self.balance + monto
         else:
-            raise ValueError(f"No es posible gastar {monto} con un balance de {self.balance}")
-        
+            raise ValueError(
+                f"No es posible gastar {monto} con un balance de {self.balance}"
+            )
+
     def depositar(self, monto):
         if monto > self.balance:
             raise ValueError(f"El monto {monto} excede el balance {self.balance}")
@@ -20,5 +25,69 @@ class TarjetaDeCredito:
             self.balance = self.balance - monto
 
     def reporte(self):
-        return (f"El cliente {self.nombre} con tarjeta {self.numero_tarjeta} tiene un balance de {self.balance} "
-                f"y un limite de {self.limite}")
+        return (
+            f"El cliente {self.nombre} con tarjeta {self.numero_tarjeta}"
+            f" tiene un balance de {self.balance} "
+            f"y un limite de {self.limite}"
+        )
+
+
+class Vector:
+
+    def __init__(self, dimension) -> None:
+        self.dimension = dimension
+        self.datos = [0 for _ in range(dimension)]
+
+    def representar(self):
+        return "(" + ",".join([str(dato) for dato in self.datos]) + ")"
+
+    def obtener_elemento(self, indice):
+        return self.datos[indice]
+
+    def asignar_elemento(self, indice, valor):
+        if indice < 0 or indice >= self.dimension:
+            raise ValueError("Indice incorrecto!")
+        self.datos[indice] = valor
+
+    def sumar(self, otro_vector):
+        if self.dimension != otro_vector.dimension:
+            raise ValueError("Las dimensiones son diferentes")
+
+        resultado = Vector(self.dimension)
+
+        for indice in range(self.dimension):
+            resultado.asignar_elemento(
+                indice,
+                self.obtener_elemento(indice) + otro_vector.obtener_elemento(indice),
+            )
+
+        return resultado
+
+    def es_igual(self, otro_vector):
+        if self.dimension != otro_vector.dimension:
+            return False
+
+        for indice in range(self.dimension):
+            if self.obtener_elemento(indice) != otro_vector.obtener_elemento(indice):
+                return False
+        return True
+
+    def producto_escalar(self, otro_vector):
+        if self.dimension != otro_vector.dimension:
+            raise ValueError("Las dimensiones son diferentes!")
+
+        resultado = 0
+        for indice in range(self.dimension):
+            resultado = resultado + self.obtener_elemento(
+                indice
+            ) * otro_vector.obtener_elemento(indice)
+
+        return resultado
+
+    def distancia_coseno(self, otro_vector):
+        numerador = self.producto_escalar(otro_vector)
+        denominador = math.sqrt(self.producto_escalar(self)) * math.sqrt(
+            otro_vector.producto_escalar(otro_vector)
+        )
+
+        return numerador / denominador
--- test_laboratorio.py
+++ test_laboratorio.py
@@ -42,3 +42,71 @@ def test_comprar_con_tarjeta():
         "El cliente Lolo Fernandez con tarjeta 123456"
         " tiene un balance de 0 y un limite de 100"
     )
+
+
+def test_crear_vector() -> None:
+    vector: laboratorio.Vector = laboratorio.Vector(3)
+
+    assert vector.dimension == 3
+    assert vector.representar() == "(0,0,0)"
+    assert vector.obtener_elemento(2) == 0
+
+    vector.asignar_elemento(2, 2)
+    assert vector.obtener_elemento(2) == 2
+
+    otro_vector: laboratorio.Vector = laboratorio.Vector(3)
+    otro_vector.asignar_elemento(0, 1)
+    otro_vector.asignar_elemento(1, 2)
+    otro_vector.asignar_elemento(2, 3)
+
+    with pytest.raises(ValueError):
+        otro_vector.asignar_elemento(-1, -1)
+
+    with pytest.raises(ValueError):
+        otro_vector.asignar_elemento(3, 4)
+
+    with pytest.raises(ValueError):
+        vector.sumar(laboratorio.Vector(2))
+
+    assert not vector.es_igual(laboratorio.Vector(1))
+
+    resultado: laboratorio.Vector = vector.sumar(otro_vector)
+    assert resultado.obtener_elemento(0) == 1
+    assert resultado.obtener_elemento(1) == 2
+    assert resultado.obtener_elemento(2) == 5
+
+    assert not resultado.es_igual(otro_vector)
+
+    vector_copia = laboratorio.Vector(3)
+    vector_copia.asignar_elemento(0, 0)
+    vector_copia.asignar_elemento(1, 0)
+    vector_copia.asignar_elemento(2, 2)
+
+    assert vector_copia.es_igual(vector)
+
+    factor = laboratorio.Vector(2)
+    factor.asignar_elemento(0, 3)
+    factor.asignar_elemento(1, 0)
+
+    otro_factor = laboratorio.Vector(2)
+    otro_factor.asignar_elemento(0, 5)
+    otro_factor.asignar_elemento(1, 5)
+
+    assert factor.producto_escalar(otro_factor) == 15
+
+    with pytest.raises(ValueError):
+        factor.producto_escalar(vector_copia)
+
+    factor = laboratorio.Vector(4)
+    factor.asignar_elemento(0, 3)
+    factor.asignar_elemento(1, 2)
+    factor.asignar_elemento(2, 0)
+    factor.asignar_elemento(3, 5)
+
+    otro_factor = laboratorio.Vector(4)
+    otro_factor.asignar_elemento(0, 1)
+    otro_factor.asignar_elemento(1, 0)
+    otro_factor.asignar_elemento(2, 0)
+    otro_factor.asignar_elemento(3, 0)
+
+    assert factor.distancia_coseno(otro_factor) == pytest.approx(0.486, rel=1e-2)
